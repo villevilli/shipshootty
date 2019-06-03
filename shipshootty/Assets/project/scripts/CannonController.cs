f2c8d3748b90b4259bc79cf936bf081b3640d8c2
Unknown <kujala.ville.vk@gmail.com>
2019-06-03 16:36:36 +0300

    did what the last commit did
    the last commit kinda broke everything had to redo everything fixed project

---
+++ shipshootty/Assets/project/scripts/CannonController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{                                      
+    public Vector3 rayCastLocation;
+
+    int floorMask;                      
+    float camRayLength = 100f;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+    	Turning();
+    }
+
+    void Turning()
+    {
+    	RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            
+            Vector3 playerToMouse = hit.point - transform.position;
+    		playerToMouse.y = 0.0f; 
+    		Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+    		rb.MoveRotation(newRotation);
+    		Debug.Log(newRotation);
+            
+        }
+    }
+}
+
